6aa466f4b22dbdf9f3b67509e7e65a676cc4d6f7
Sourav-xd <souravjoshi1607@gmail.com>
2026-01-08 17:52:59 +0530

    Dev-branch commit-1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.api.websocket_routes import router as websocket_router
 from app.api.interview_routes import router as interview_router
 
 app = FastAPI()
-
 app.include_router(interview_router)
 app.include_router(websocket_router)
 
